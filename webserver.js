e03e537318b4fc248fe4c58f56d860f98d06fcf4
G. Preethi <preethisai597@gmail.com>
2025-03-16 13:32:39 +1100

    Testing my webserver code

--- webserver.js
+++ webserver.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static HTML file
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
